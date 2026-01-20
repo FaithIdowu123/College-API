8a6d738996bc7a584641ab7951d910b1188fe6f8
ofaith@byupathway.edu <ofaith@byupathway.edu>
2026-01-20 02:43:26 +0100

    check fix

--- utilities/course.js
+++ utilities/course.js
@@ -1,7 +1,14 @@
 const Joi = require('joi');
 
 const addStudentSchema = Joi.object({
-  studentId: Joi.string().min(3).max(20).required()
+  studentId: Joi.string().min(3).max(20).required().external(async (value) => {
+    const db = getDB();
+    const student = await db.collection('Students').findOne({ studentId: value });
+    if (!student) {
+      throw new Error(`Student with ID ${value} does not exist`);
+    }
+    return value; // must return the value
+  })
 });
 
 module.exports = {
